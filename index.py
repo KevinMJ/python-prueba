624e9f0b7bb657f5da3ab2e9292775fd95fdcee0
Kevin MJ <kevinmelendezjuarez@gmail.com>
2024-04-25 14:25:23 -0600

    egundo cambio

---
+++ index.py
@@ -0,0 +1,16 @@
+lista = [1, 2, 3]
+print(lista)
+lista.append(4) #Este método añade al final de la lista
+# lista.clear() Elimina los elementos de la lista
+# lista.copy() copia los elementos de la lista en una nueva pero no iguala las referencias en memoria
+print(lista)
+lista2 = lista.copy()
+# El metodo lista.count() cuenta el número de veces que apareces el elemento etre paréntesis en la lista
+# La función len() muestra el tamaño de elemetos de la lista
+print(len(lista))
+# Para acceder a un elemento podemos usar la notación lista[0] se llama método de acceso por índices
+# lista.pop() elimina el último elemento de la lista
+# lista.remove(elemento a eliminar) elimina el elemento seleccionado de la lista.
+# lista-append(4) agrega el valor 4 a la lista.
+# len(lista) devuelve el tamaño de la lista
+# podemos acceder a los valores de una lista por medio de índices lista[0], muestra el primer elemento de la lista.
