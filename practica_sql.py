a3d5bb2ec9442c2d7c4e5f94d08938b39b893cec
Kevin MJ <kevinjuarezmelendez572@gmail.com>
2024-10-31 15:45:36 -0600

    ubuntu

--- practica_sql.py
+++ practica_sql.py
@@ -3,12 +3,21 @@ import mysql.connector
 conexion = mysql.connector.connect(
     host = "localhost",
     user="root",
-    password = "B14niet-"
+    password = "B14niet-",
+    database = "pruebas"
 )
 
 cursor = conexion.cursor()
 
-cursor.execute("SHOW DATABASES")
+try:
+    cursor.execute("CREATE TABLE clientes"
+                    "(id INT NOT NULL AUTO_INCREMENT,"
+                    "nombre VARCHAR (32) NOT NULL,"
+                    "apellidos VARCHAR (64) NOT NULL,"
+                    "telefono VARCHAR (9) NOT NULL,"
+                    "direccion VARCHAR (256),"
+                    "PRIMARY KEY (id));")   
+    print("se creó correctamente la tabla.")
 
-for bd in cursor:
-    print(bd)
+except:
+    print("Ocurrió un error al intentar crear la tabla. Inténtelo nuevamente")
